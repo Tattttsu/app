23203ab6410ec7c228e8407558528cdb406a63b6
Ubuntu <ubuntu@ip-172-31-45-28.ap-northeast-3.compute.internal>
2022-06-01 13:33:19 +0000

    10章削除前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
    include SessionsHelper
     before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
     
     
   def new
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @index = User.all
+    @users = User.paginate(page: params[:page])
   end
   
   def show
@@ -52,6 +53,8 @@ class UsersController < ApplicationController
     #beforeアクション　
     def logged_in_user
       unless logged_in?
+      store_location
+        
       flash[:danger] = "log inしてください"
       redirect_to login_url
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_014739) do
+ActiveRecord::Schema.define(version: 2022_06_01_130241) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_014739) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
   end
 
 end
