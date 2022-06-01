d035e629804b5e2dea106db27ea757ab48d37a77
Ubuntu <ubuntu@ip-172-31-45-28.ap-northeast-3.compute.internal>
2022-06-01 12:07:40 +0000

    9章終わり

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+   include SessionsHelper
+    before_action :logged_in_user, only: [:edit, :update]
+    
+    
+  def new
+    @user = User.new
+  end
+  
+  def home
+  end
+  
+  def index
+    @index = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+  
+  def edit
+    @user = User.find_by(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      log_in @user
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+        
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :rank, :password, :password_confirmation)
+    end
+    
+    #beforeアクション　
+    def logged_in_user
+      unless logged_in?
+      flash[:danger] = "log inしてください"
+      redirect_to login_url
+      end
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+
+  get 'sessions/new'
+  root 'users#home'
+
+  get "/signup", to:"users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+
+  
+  resources :users
+  
+end
